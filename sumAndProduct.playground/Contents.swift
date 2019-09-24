8df446e5085f692d7be6dfbd40960631ae880716
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-09-23 21:34:51 -0400

    Completed challenge

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,15 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
+    var results: [UInt] = []
+    var y: UInt = 1
+    for x: UInt in 1..<product {
+        if x + y == sum && x * y == product {
+        results.append(y)
+        results.append(x)
+        }
+        y += 1        }
+    return results    }
     
-}
+
+sumAndProduct(6, 9)
