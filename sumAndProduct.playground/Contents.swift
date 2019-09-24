f6a0a330be0aca3ad88e84c58baebe8b48bb0bc9
JosephRogers94 <Joerogersss2016@gmail.com>
2019-09-23 20:21:42 -0500

    Edge Casing Resolved small bug

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
     var newArray: [UInt] = []
     for n in 1..<sum {
-        if n + n == sum || n * n == product{
+        if n + n == sum || n * n == product && n != 0 {
             newArray.append(n)
         }
     }
