2fc50bba4c6ca0e7b51c28be75d0566d73df1547
Bree <bree.j.32@gmail.com>
2019-09-23 21:28:04 -0400

    write function

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,16 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
-    
+    for x in sum {
+        if x % 2 == 0 {
+            print()
+            
+        }
+    }
 }
+
+
+
+var sum = 6
+
+var product = 12
