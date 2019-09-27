f63ac3564b55f7f53eee1dc63b512c79cb11f4da
Vici Shaweddy <vshaweddy@gmail.com>
2019-09-26 21:38:24 -0400

    completed the challenge

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,20 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
-    
+    for x in 1..<sum {
+        for y in 1..<sum {
+            if x + y == sum && x * y == product {
+                return [x, y]
+            }
+        }
+    }
+    return []
 }
+
+// Test Cases
+sumAndProduct(6, 9)
+sumAndProduct(3, 5)
+sumAndProduct(8, 16)
+sumAndProduct(100, 10)
+sumAndProduct(2, 1)
+sumAndProduct(4, 4)
