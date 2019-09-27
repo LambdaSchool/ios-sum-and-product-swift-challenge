cd2a0f8a9b69ff68f4da434f8848ccbfb98eed58
dillonp23 <dillonp.lambda@gmail.com>
2019-09-26 20:40:41 -0700

    Reduce code

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,22 +1,20 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
-    var a: UInt = 0
     var result: [UInt] = []
+    var a = UInt()
     
     for x in 1...sum {
-        if sum % x == 0 {
-            if product % x == 0 {
+        if sum % x == 0 && product % x == 0 {
                 a = product / x
                 if x + a == sum {
                     result.append(a)
                     result.append(x)
                     break
-                }
             }
         }
     }
     return result.sorted()
 }
 
-sumAndProduct(5, 225)
+sumAndProduct(8, 12)
