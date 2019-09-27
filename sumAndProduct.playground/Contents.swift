6213a41d605087411cb3d462e589b4386ab6688f
dillonp23 <dillonp.lambda@gmail.com>
2019-09-26 19:02:09 -0700

    Working solution

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,34 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
+    var count: UInt = 1
+    var a: UInt = 0
+    var b: UInt = 0
+    var result: [UInt] = []
     
+    for _ in 1...sum {
+        if sum % count == 0 {
+            a = count
+            if product % a == 0 {
+                b = product / a
+            }
+            if a + b == sum {
+                result.append(a)
+                result.append(b)
+                break
+            } else {
+                count += 1
+            }
+        } else {
+            count += 1
+        }
+    }
+    
+    if result.count <= 1 {
+        result = []
+    }
+    
+    return result
 }
+
+sumAndProduct(8, 12)
