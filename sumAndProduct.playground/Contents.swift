1fcae2c4918c525752aea41f1e2141313aa99493
Jesse Ruiz <jesseleeruiz@gmail.com>
2019-10-01 05:21:00 -1000

    Answered Challenge

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,22 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
+    var array: [UInt] = []
+    let num = sum
+    let newSumNum = num / 2
+    let answerSum = newSumNum + newSumNum
+    let answerPro = newSumNum * newSumNum
+    if answerSum == sum && answerPro == product {
+        array.append(answerSum)
+        array.append(answerPro)
+    } else {
+        print("These values did not work.")
+    }
     
+    return array
 }
+
+sumAndProduct(4, 4)
+sumAndProduct(6, 9)
+sumAndProduct(8, 16)
+sumAndProduct(10, 25)
