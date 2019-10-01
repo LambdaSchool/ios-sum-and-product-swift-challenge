e6ac053d0fc37bd8ced28d4cf25ae5c6c60789fa
Wilma <kima_92@fastmail.com>
2019-10-01 11:25:26 -0400

    Completed sumAndProduct Coding Challenge

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,23 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
+    var x: UInt = 0
+    var y: UInt = 0
+    var xyArray: [UInt] = []
     
+    for a in 0...100 {
+        for b in 0...100 {
+            if a + b == sum && a * b == product {
+                x = UInt(a)
+                y = UInt(b)
+                
+            }
+        }
+        
+    }
+    xyArray.append(x)
+    xyArray.append(y)
+    return xyArray.sorted()
 }
+
+sumAndProduct(6, 9)
