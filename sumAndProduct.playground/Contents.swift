c608144015a7dc46797d67fd9a0823c5d2e1461b
Isvvc <lyons@tuta.io>
2019-10-01 09:34:03 -0600

    Implement sumAndProduct()

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,24 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
+    var a: UInt
+    var b: UInt
     
+    let halfSum: UInt = UInt(round(Float(sum) / 2.0))
+    
+    for i in 0...halfSum {
+        a = i
+        b = sum - i
+        if a * b == product {
+            return [a,b]
+        }
+    }
+    
+    return []
 }
+
+print(sumAndProduct(6, 9))
+print(sumAndProduct(21, 108))
+print(sumAndProduct(120, 3276))
+print(sumAndProduct(580245, 56393342784))
+print(sumAndProduct(1, 3))
