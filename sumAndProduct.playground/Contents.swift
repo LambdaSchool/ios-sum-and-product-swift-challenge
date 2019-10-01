e5a1b9e3135dc75496f3668c0cd5963be2398292
Andrew Ruiz <nukedoom@gmail.com>
2019-10-01 09:02:09 -0700

    Final

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,6 +1,25 @@
 import UIKit
 
-// Testing
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
-    let foo = 1
+    
+    if sum > 0 && product > 0 {
+        
+        var sumTuple : (UInt,UInt)
+        
+        // sum/2 because we would repeat ourselves otherwise when counting the operands of a sum
+        for index in 1...sum/2 {
+            sumTuple.0 = index
+            sumTuple.1 = sum - index
+            // print(sumTuple)
+            
+            if sumTuple.0 * sumTuple.1 == product {
+                return [sumTuple.0, sumTuple.1]
+            }
+        }
+    }
+    
+    return []
 }
+
+// Testing
+sumAndProduct(6, 9)
