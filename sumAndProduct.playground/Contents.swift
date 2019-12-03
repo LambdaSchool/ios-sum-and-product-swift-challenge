633054545f3774a78ff5f9b5b4ff77362f98e737
alfcolon <alfcolon@gmail.com>
2019-12-02 19:21:33 -0800

    Challenge answer

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,30 @@
 import UIKit
+//### CHALLENGE
+// - Write a function - sumAndProduct() - that takes two positive integers, a sum and a product, and returns the smallest two positive integers x and y, where x + y == sum, and x * y == product.
+// - Return x and y in an array with the smaller number first in the format [x, y]
+// - If a solution cannot be found, return the empty array
+ 
+ // Example: sumAndProduct(6, 9) should return [3, 3] because 3 + 3 == 6, and 3 * 3 == 9.```
+
+/*Here are the overall [instructions for code challenges](https://github.com/LambdaSchool/ios-code-challenge-instructions). Before you begin, fork and clone this repo and work through your solution in the included starter playground file. When you're done, **Please make sure to save and push all your work, and submit a Pull Request. Don't forget tag your TL so they can review your submission!**/
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
     
+    if sum == 0 || product == 0{
+        return [0, sum < product ? sum: product]
+    }
+    var x: UInt!
+    var y: UInt!
+    for n in 0...sum{
+        for m in 0...product{
+            if n + m == sum{
+                if n * m == product{
+                    x = n
+                    y = m
+                }
+            }
+        }
+    }
+    return x < y ? [x, y] : [y, x]
 }
+sumAndProduct(6, 9)
