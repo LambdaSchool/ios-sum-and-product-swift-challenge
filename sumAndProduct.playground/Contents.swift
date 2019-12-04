add5b88813ddf514b4ad3814e1f454185aa52a67
Patrick Millet <lohs79seahawk@gmail.com>
2019-12-04 10:52:38 -0600

    completed func

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,21 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
+    var result: [UInt] = []
     
+    var y: UInt = 1
+    
+    for x: UInt in 1..<product {
+        if x + y == sum && x * y == product {
+            result.append(x)
+            result.append(y)
+        }
+        y += 1
+    }
+    return result
 }
+
+
+sumAndProduct(6, 9)
+sumAndProduct(12, 18)
+sumAndProduct(1200, 2400)
