139e4a271dab6ddca224179970833a8c3763df37
Angelique Abacajan <mangelique9800@gmail.com>
2019-12-04 11:49:56 -0500

    Completed Code Challenge

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,20 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
-    
+    var a: UInt
+    var b: UInt
+
+    let halfSum: UInt = UInt(round(Float(sum) / 2.0))
+
+    for i in 0...halfSum {
+        a = i
+        b = sum - i
+        if a * b == product {
+            return [a,b]
+        }
+    }
+
+    return []
 }
+
+print(sumAndProduct(6, 9))
